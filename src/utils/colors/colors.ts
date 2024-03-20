96f98d94b4c1dadbd4319f7ed910a5511a9dbcaf
tundenewcodes <tundef@eraconneckt.com>
2024-03-20 20:08:00 +0100

    first commit

---
+++ src/utils/colors/colors.ts
@@ -0,0 +1,14 @@
+export const Colors = () => {
+  const whiteColor: string = "#fff", // white
+    blackColor: string = "#000000",
+    lightDarkColor: string = "#161616",
+    goldColor: string = "#D1B34F";
+
+  return {
+    whiteColor,
+
+    blackColor,
+    lightDarkColor,
+    goldColor,
+  };
+};
